8ac576e1d0b2d2bc418c99b1ec78fa1964a7f0ce
joqtan <joqtanmarmol@gmail.com>
2021-03-19 17:33:26 -0600

    takeCard and CardValue funct

--- assets/js/game.js
+++ assets/js/game.js
@@ -20,9 +20,36 @@ const createDeck = () => {
             deck.push(special + type)
         }
     }
-    console.log(deck)
+    // console.log(deck)
     deck = _.shuffle(deck)
     console.log(deck)
     return deck
 }
 createDeck()
+
+const takeCard = () => {
+    if (deck.length === 0) {
+        throw 'Deck empty'
+    }
+    let card = deck.pop()
+    console.log(card)
+    console.log(deck)
+    return card
+}
+
+// const cardValue = (card) => {
+//     const value = card.substring(0, card.length - 1)
+//     let points = 0
+//     if (isNaN(value)) {
+//         points = value === 'A' ? 11 : 10
+//     } else {
+//         points = value * 1
+//     }
+//     console.log(points)
+// }
+const cardValue = (card) => {
+    const value = card.substring(0, card.length - 1)
+    return isNaN(value) ? (value === 'A' ? 11 : 10) : value * 1
+}
+const value = cardValue(takeCard())
+console.log({ value })
