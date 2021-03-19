923735c77b0347aafe793a0c70b7a52171abf1cb
joqtan <joqtanmarmol@gmail.com>
2021-03-19 16:45:53 -0600

    first commit

---
+++ assets/js/game.js
@@ -0,0 +1,28 @@
+/*
+clubs
+diamonds
+hearts
+spades
+*/
+
+let deck = []
+const types = ['C', 'D', 'H', 'S']
+const specials = ['A', 'J', 'Q', 'K']
+
+const createDeck = () => {
+    for (let i = 2; i <= 10; i++) {
+        for (let type of types) {
+            deck.push(i + type)
+        }
+    }
+    for (let type of types) {
+        for (let special of specials) {
+            deck.push(special + type)
+        }
+    }
+    console.log(deck)
+    deck = _.shuffle(deck)
+    console.log(deck)
+    return deck
+}
+createDeck()
