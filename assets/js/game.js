6cd957b4f82634f042664228eb388074818456d5
joqtan <joqtanmarmol@gmail.com>
2021-03-21 17:22:35 -0600

    player cards and computer 'logic'

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,6 +8,15 @@ spades
 let deck = []
 const types = ['C', 'D', 'H', 'S']
 const specials = ['A', 'J', 'Q', 'K']
+let playerPoints = 0,
+    computerPoints = 0
+
+/*html references */
+const btnTake = document.querySelector('#btn-take')
+const btnstop = document.querySelector('#btn-stop')
+const divPlayerCards = document.querySelector('#player-cards')
+const divComputerCards = document.querySelector('#computer-cards')
+const poinstSmall = document.querySelectorAll('small')
 
 const createDeck = () => {
     for (let i = 2; i <= 10; i++) {
@@ -22,9 +31,9 @@ const createDeck = () => {
     }
     // console.log(deck)
     deck = _.shuffle(deck)
-    console.log(deck)
     return deck
 }
+
 createDeck()
 
 const takeCard = () => {
@@ -32,24 +41,58 @@ const takeCard = () => {
         throw 'Deck empty'
     }
     let card = deck.pop()
-    console.log(card)
-    console.log(deck)
     return card
 }
 
-// const cardValue = (card) => {
-//     const value = card.substring(0, card.length - 1)
-//     let points = 0
-//     if (isNaN(value)) {
-//         points = value === 'A' ? 11 : 10
-//     } else {
-//         points = value * 1
-//     }
-//     console.log(points)
-// }
 const cardValue = (card) => {
     const value = card.substring(0, card.length - 1)
     return isNaN(value) ? (value === 'A' ? 11 : 10) : value * 1
 }
-const value = cardValue(takeCard())
-console.log({ value })
+
+//computer
+
+const computerTurn = (minPoints) => {
+    do {
+        const card = takeCard()
+        computerPoints += cardValue(card)
+        poinstSmall[1].innerText = computerPoints
+
+        const imgCard = document.createElement('img')
+        imgCard.classList.add('cards')
+        imgCard.src = `assets/cards/${card}.png`
+        divComputerCards.append(imgCard)
+        if (minPoints > 21) {
+            break
+        }
+    } while (computerPoints < minPoints && minPoints <= 21)
+}
+/*
+events
+*/
+btnTake.addEventListener('click', () => {
+    const card = takeCard()
+    playerPoints += cardValue(card)
+    poinstSmall[0].innerText = playerPoints
+    const imgCard = document.createElement('img')
+    imgCard.classList.add('cards')
+    imgCard.src = `assets/cards/${card}.png`
+    divPlayerCards.append(imgCard)
+
+    if (playerPoints > 21) {
+        console.warn('you lost')
+        btnTake.disabled = true
+        btnstop.disabled = true
+        computerTurn(playerPoints)
+    } else if (playerPoints === 21) {
+        console.warn('great, 21')
+        btnTake.disabled = true
+        btnstop.disabled = true
+        computerTurn(playerPoints)
+    }
+})
+
+btnstop.addEventListener('click', () => {
+    btnTake.disabled = true
+    btnstop.disabled = true
+    computerTurn(playerPoints)
+})
